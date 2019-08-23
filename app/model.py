4f05af077449f378ebe5f5310eb2a0ab7455ad79
AhnSeongHyun <sh84.ahn@gmail.com>
2019-08-23 20:26:11 +0900

    Add masking

--- app/model.py
+++ app/model.py
@@ -9,12 +9,21 @@ class EncryptedField(TypeDecorator):
     impl = VARCHAR
 
     def process_bind_param(self, value, dialect):
-        print(f'process_bind_param : {value} {encrypt(value)}')
         return encrypt(value)
 
     def process_result_value(self, value, dialect):
-        print(f'process_result_value : {value} {decrypt(value)}')
         return decrypt(value)
+
+
+class MaskingPhoneNumberField(TypeDecorator):
+    impl = VARCHAR
+
+    def process_bind_param(self, value, dialect):
+        return encrypt(value)
+
+    def process_result_value(self, value, dialect):
+        v = decrypt(value)
+        return f'{v[:3]}****{v[7:]}'
 
 
 class User(db.Model):
@@ -24,4 +33,4 @@ class User(db.Model):
     user_id = Column(BigInteger, primary_key=True, autoincrement=True)
     name = Column(EncryptedField(128), nullable=False)
     birthday = Column(EncryptedField(128), nullable=False)
-    phone_number = Column(EncryptedField(128), nullable=False)
+    phone_number = Column(MaskingPhoneNumberField(128), nullable=False)
